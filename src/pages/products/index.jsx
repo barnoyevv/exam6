9761b88b149b0d176786bf0876ad5f824da7b0c8
Shohjahon <shohjahon@MacBook-Air-Shohjahon.local>
2024-06-10 18:30:55 +0500

    first commit

--- src/pages/products/index.jsx
+++ src/pages/products/index.jsx
@@ -6,121 +6,124 @@ import Select from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
 import InputLabel from "@mui/material/InputLabel";
 import FormControl from "@mui/material/FormControl";
+import axios from "axios";
+import StarIcon from '@mui/icons-material/Star';
+import "./index.css"
 
-  
+function Users() {
+	const [users, setUsers] = useState([])
+	const [filteredProducts, setFilteredProducts] = useState([]);
+	const [category, setCategory] = useState("");
+	const [itemsPerPage, setItemsPerPage] = useState(3);
+	const [page, setPage] = useState(1);
 
-function Products() {
-  const [products, setProducts] = useState([]);
-  const [filteredProducts, setFilteredProducts] = useState([]);
-  const [category, setCategory] = useState("");
-  const [rating, setRating] = useState("");
-  const [itemsPerPage, setItemsPerPage] = useState(3);
-  const [page, setPage] = useState(1);
 
-  useEffect(() => {
-    const filtered = products.filter(product => {
-      const categoryMatch = category ? product.category === category : true;
-      const ratingMatch = rating ? product.rating.rate >= rating : true;
-      return categoryMatch && ratingMatch;
-    });
-    setFilteredProducts(filtered);
-    setPage(1);
-  }, [category, rating, products]);
 
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
+	useEffect(() => {
+		axios.get(`https://fakestoreapi.com/products?_page=1&_limit=2`).then(response => {
+			setUsers(response.data)
+		})
+	}, [])
 
-  const handleCategoryChange = event => {
-    setCategory(event.target.value);
-  };
+	useEffect(() => {
+		const filtered = users.filter(item => {
+			const categoryMatch = category ? item.category === category : true;
+			return categoryMatch;
+		});
+		setFilteredProducts(filtered);
+		setPage(1);
+	}, [category, users]);
 
-  const handleRatingChange = event => {
-    setRating(event.target.value);
-  };
+	const handleChangePage = (event, newPage) => {
+		setPage(newPage);
+	};
 
-  const handleItemsPerPageChange = event => {
-    setItemsPerPage(event.target.value);
-    setPage(1);
-  };
+	const handleCategoryChange = event => {
+		setCategory(event.target.value);
+	};
 
-  const pageCount = Math.ceil(filteredProducts.length / itemsPerPage);
+	const handleItemsPerPageChange = event => {
+		setItemsPerPage(event.target.value);
+		setPage(1);
+	};
 
-  const startIndex = (page - 1) * itemsPerPage;
-  const endIndex = Math.min(startIndex + itemsPerPage, filteredProducts.length);
+	const pageCount = Math.ceil(filteredProducts.length / itemsPerPage);
 
-  const displayedProducts = filteredProducts.slice(startIndex, endIndex);
+	const startIndex = (page - 1) * itemsPerPage;
+	const endIndex = Math.min(startIndex + itemsPerPage, filteredProducts.length);
 
-  return (
-    <div>
-      <div className="FormControl1">
-        <FormControl variant="outlined" fullWidth>
-          <InputLabel>Category</InputLabel>
-          <Select
-            value={category}
-            onChange={handleCategoryChange}
-            label="Category"
-          >
-            <MenuItem value="">All</MenuItem>
-            <MenuItem value="men's clothing">Men's Clothing</MenuItem>
-            <MenuItem value="women's clothing">Women's Clothing</MenuItem>
-            <MenuItem value="jewelery">Jewelery</MenuItem>
-            <MenuItem value="electronics">Electronics</MenuItem>
-          </Select>
-        </FormControl>
-      </div>
-      <div className="FormContro2">
-        <FormControl variant="outlined" fullWidth>
-          <InputLabel>Items per Page</InputLabel>
-          <Select
-            value={itemsPerPage}
-            onChange={handleItemsPerPageChange}
-            label="Items per Page"
-          >
-            <MenuItem value={3}>3</MenuItem>
-            <MenuItem value={5}>5</MenuItem>
-            <MenuItem value={10}>10</MenuItem>
-          </Select>
-        </FormControl>
-      </div>
-      <div className="products-list">
-        {displayedProducts.map(product => (
-          <div key={product.id} className="product-card">
-            <Link to={`/product/${product.id}`}>
-              <h3>{product.title}</h3>
-            </Link>
-            <p>
-              <strong>Price:</strong> ${product.price}
-            </p>
-            <p>
-              <strong>Description:</strong> {product.description}
-            </p>
-            <p>
-              <strong>Category:</strong> {product.category}
-            </p>
-            <p>
-              <strong>Rating:</strong> {product.rating.rate} (
-              {product.rating.count} reviews)
-            </p>
-            <img
-              src={product.image}
-              alt={product.title}
-              className="product-image"
-            />
-          </div>
-        ))}
-      </div>
-      <Stack spacing={2}>
-        <Pagination
-          count={pageCount}
-          page={page}
-          onChange={handleChangePage}
-          color="primary"
-          variant="outlined"
-        />
-      </Stack>
-    </div>
-  );
+	const displayedProducts = filteredProducts.slice(startIndex, endIndex);
+
+	return (
+		<div>
+			<div className="FormContro2">
+			<FormControl variant="outlined" fullWidth>
+					<InputLabel>Category</InputLabel>
+					<Select
+						value={category}
+						onChange={handleCategoryChange}
+						label="Category"
+					>
+						<MenuItem value="">All</MenuItem>
+						<MenuItem value="men's clothing">Men's Clothing</MenuItem>
+						<MenuItem value="women's clothing">Women's Clothing</MenuItem>
+						<MenuItem value="jewelery">Jewelery</MenuItem>
+						<MenuItem value="electronics">Electronics</MenuItem>
+					</Select>
+				</FormControl>
+				<FormControl variant="outlined" fullWidth className="my-3">
+					<InputLabel>Limit</InputLabel>
+					<Select
+						value={itemsPerPage}
+						onChange={handleItemsPerPageChange}
+						label="Limit"
+					>
+						<MenuItem value={3}>3</MenuItem>
+						<MenuItem value={5}>5</MenuItem>
+						<MenuItem value={10}>10</MenuItem>
+					</Select>
+				</FormControl>
+			</div>
+			<div className="products-list">
+				{displayedProducts.map(item => (
+					<div key={item.id} className="product-card">
+						<Link to={`/product/${item.id}`}>
+							<div className="card">
+								<div className="card-header bg-primary">
+									<h3>{item.title}</h3>
+								</div>
+								<div className="card-body">
+									<img
+										src={item.image}
+										alt={item.title}
+										className="product-image"
+									/>
+								</div>
+								<div className="card-footer">
+									<p>{item.description}</p>
+									<div className="rate">
+										<span><StarIcon /></span>
+										<span>{item.rating.rate}</span>
+									</div>
+									<strong>{item.price}$</strong>
+								</div>
+							</div>
+
+						</Link>
+					</div>
+				))}
+			</div>
+			<Stack spacing={2}>
+				<Pagination
+					count={pageCount}
+					page={page}
+					onChange={handleChangePage}
+					color="secondary"
+					variant="outlined"
+				/>
+			</Stack>
+		</div>
+	);
 }
 
-export default Products;
+export default Users;
